cd857a359143fca3cca48f93bdc25e1140361b4c
Mazy <mazymohamed06@gmail.com>
2022-08-21 20:38:14 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes , Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes , Route } from 'react-router-dom';
 import { useContext } from 'react'
 import { ThemeContext } from './contexts/theme'
 import Header from './components/Header/Header'
