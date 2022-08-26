cdbe2a6d1006eb2a802e15d0e7bb34dee6b8007a
Mazy <mazymohamed06@gmail.com>
2022-08-26 13:23:34 +0100

    iframe and back button deleted

--- src/components/BackToPortfolio/BackToPortfolio.js
+++ src/components/BackToPortfolio/BackToPortfolio.js
@@ -1,32 +1,32 @@
-import './BackToPortfolio.css'
+// import './BackToPortfolio.css'
 
-const BackToPortfolio = () => (
-  <section className='centered-container'>
-    <a className='link link--arrowed' href='/'>
-      <svg
-        className='arrow-icon'
-        xmlns='http://www.w3.org/2000/svg'
-        width='32'
-        height='32'
-        viewBox='0 0 32 32'
-      >
-        <g
-          fill='none'
-          stroke='black'
-          strokeWidth='1.5'
-          strokeLinejoin='round'
-          strokeMiterlimit='10'
-        >
-          <circle className='arrow-icon--circle' cx='16' cy='16' r='15.12' />
-          <path
-            className='arrow-icon--arrow'
-            d='M16.14 9.93L22.21 16l-6.07 6.07M8.23 16h13.98'
-          />
-        </g>
-      </svg>
-      Back to portfolio
-    </a>
-  </section>
-)
+// const BackToPortfolio = () => (
+//   <section className='centered-container'>
+//     <a className='link link--arrowed' href='/'>
+//       <svg
+//         className='arrow-icon'
+//         xmlns='http://www.w3.org/2000/svg'
+//         width='32'
+//         height='32'
+//         viewBox='0 0 32 32'
+//       >
+//         <g
+//           fill='none'
+//           stroke='black'
+//           strokeWidth='1.5'
+//           strokeLinejoin='round'
+//           strokeMiterlimit='10'
+//         >
+//           <circle className='arrow-icon--circle' cx='16' cy='16' r='15.12' />
+//           <path
+//             className='arrow-icon--arrow'
+//             d='M16.14 9.93L22.21 16l-6.07 6.07M8.23 16h13.98'
+//           />
+//         </g>
+//       </svg>
+//       Back to portfolio
+//     </a>
+//   </section>
+// )
 
-export default BackToPortfolio
+// export default BackToPortfolio
--- src/components/Iframe/Iframe.js
+++ src/components/Iframe/Iframe.js
@@ -1,52 +1,52 @@
-import  { Navigate } from 'react-router-dom'
-import './Iframe.css'
-import BackToPortfolio from '../BackToPortfolio/BackToPortfolio'
-import { projects } from '../../portfolio'
+// import  { Navigate } from 'react-router-dom'
+// import './Iframe.css'
+// import BackToPortfolio from '../BackToPortfolio/BackToPortfolio'
+// import { projects } from '../../portfolio'
 
 
 
-const Iframe = () => {
+// const Iframe = () => {
 
-    const currentProject = window.location.pathname
-    let url = null
-    projects.map((project) => {
-        if (project.livePreview === currentProject) {
-            url = `${project.streamlitUrl}?embedded=true`
-        }
-        return <Navigate to='/' />
-    })
+//     const currentProject = window.location.pathname
+//     let url = null
+//     projects.map((project) => {
+//         if (project.livePreview === currentProject) {
+//             url = `${project.streamlitUrl}?embedded=true`
+//         }
+//         return <Navigate to='/' />
+//     })
 
-    document.body.style.overflow = "hidden";
+//     document.body.style.overflow = "hidden";
     
-    let isSafari = navigator.userAgent.indexOf("Safari") > -1;
-    // Chrome has Safari in the user agent so we need to filter (https://stackoverflow.com/a/7768006/1502448)
-    const isChrome = navigator.userAgent.indexOf('Chrome') > -1;
-    if ((isChrome) && (isSafari)) {isSafari = false;}  
-    if (isSafari) {
-        window.location.href = url;
-    }
+//     let isSafari = navigator.userAgent.indexOf("Safari") > -1;
+//     // Chrome has Safari in the user agent so we need to filter (https://stackoverflow.com/a/7768006/1502448)
+//     const isChrome = navigator.userAgent.indexOf('Chrome') > -1;
+//     if ((isChrome) && (isSafari)) {isSafari = false;}  
+//     if (isSafari) {
+//         window.location.href = url;
+//     }
 
-    const div =(
-         <div  className="holder">
-            <div className="bar">
-                <BackToPortfolio />
-            </div>
-           <iframe src={url}
-                id="streamlitApp"
-                frameBorder="0"
-                marginHeight="0"
-                marginWidth="0"
-                width="100%"
-                height="100%"
-                scrolling="auto"
-                title='StreamlitApp'
-                className="frame" />
-         </div>
-         )
+//     const div =(
+//          <div  className="holder">
+//             <div className="bar">
+//                 <BackToPortfolio />
+//             </div>
+//            <iframe src={url}
+//                 id="streamlitApp"
+//                 frameBorder="0"
+//                 marginHeight="0"
+//                 marginWidth="0"
+//                 width="100%"
+//                 height="100%"
+//                 scrolling="auto"
+//                 title='StreamlitApp'
+//                 className="frame" />
+//          </div>
+//          )
   
-    return div
-}
+//     return div
+// }
 
 
 
-export default Iframe
+// export default Iframe
